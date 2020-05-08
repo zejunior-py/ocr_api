2c651a4858019f3bc772341e3ee85f6159a2ab85
Jose Carlos Jr <jose.sysadm@gmail.com>
2020-05-08 15:48:38 -0300

    init codedeploy aws

--- start_server.sh
+++ start_server.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl libbz2-devsudo apt-get install python3-pip
-sudo pip3 install virtualenv
-mkdir downloads && cd downloads
-curl -O https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
-tar -xf Python-3.7.3.tar.xz
-cd Python-3.7.3
-./configure --enable-optimizations
-make && sudo make altinstall
+wget -qO- https://get.docker.com | sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
